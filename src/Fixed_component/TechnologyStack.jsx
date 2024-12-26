7a17f178b9017202cbc2d2ce0504050b262fbfc3
Md-Sifat-code <codewithsifat4@gmail.com>
2024-12-26 10:53:11 +0600

    addded

--- src/Fixed_component/TechnologyStack.jsx
+++ src/Fixed_component/TechnologyStack.jsx
@@ -27,104 +27,156 @@ export default function TechnologyStack() {
           our clients.
         </p>
 
-        <div className="space-y-16">
-          {/* Frontend Section */}
-          <div>
-            <h2 className="text-3xl font-bold mb-8 text-gray-200">
-              Frontend Technologies
-            </h2>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 overflow-hidden">
-              {[
-                {
-                  icon: <FaReact size={36} className="text-blue-500" />,
-                  name: "React.js",
-                },
-                {
-                  icon: <SiTailwindcss size={36} className="text-teal-500" />,
-                  name: "Tailwind CSS",
-                },
-                {
-                  icon: <FaHtml5 size={36} className="text-orange-500" />,
-                  name: "HTML5",
-                },
-                {
-                  icon: <FaCss3Alt size={36} className="text-blue-500" />,
-                  name: "CSS",
-                },
-                {
-                  icon: <FaJs size={36} className="text-yellow-500" />,
-                  name: "JavaScript",
-                },
-                {
-                  icon: <SiTypescript size={36} className="text-blue-500" />,
-                  name: "TypeScript",
-                },
-                {
-                  icon: <BiCodeAlt size={36} className="text-purple-500" />,
-                  name: "Daisy UI",
-                },
-              ].map((tech, index) => (
-                <div
-                  key={index}
-                  className="flex flex-col items-center space-y-2 p-4 rounded-lg bg-[#0f041a] hover:scale-105 transform transition-all duration-300 overflow-hidden"
-                >
-                  {tech.icon}
-                  <span className="font-medium text-gray-300">{tech.name}</span>
-                </div>
-              ))}
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 overflow-hidden">
+          {/* Technologies */}
+          {[
+            {
+              icon: <FaReact size={36} className="text-blue-500" />,
+              name: "React.js",
+            },
+            {
+              icon: <SiTailwindcss size={36} className="text-teal-500" />,
+              name: "Tailwind CSS",
+            },
+            {
+              icon: <FaHtml5 size={36} className="text-orange-500" />,
+              name: "HTML5",
+            },
+            {
+              icon: <FaCss3Alt size={36} className="text-blue-500" />,
+              name: "CSS",
+            },
+            {
+              icon: <FaJs size={36} className="text-yellow-500" />,
+              name: "JavaScript",
+            },
+            {
+              icon: <SiTypescript size={36} className="text-blue-500" />,
+              name: "TypeScript",
+            },
+            {
+              icon: <BiCodeAlt size={36} className="text-purple-500" />,
+              name: "Daisy UI",
+            },
+            {
+              icon: (
+                <img
+                  src="https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png"
+                  alt="Flutter"
+                  className="w-12 h-12"
+                />
+              ),
+              name: "Flutter",
+            },
+            {
+              icon: (
+                <img
+                  src="https://famaash.com/system/files/2024-04/square_0.png"
+                  alt="Three.js"
+                  className="w-12 h-12 object-contain"
+                />
+              ),
+              name: "Three.js",
+            },
+            {
+              icon: (
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1200px-Bootstrap_logo.svg.png"
+                  alt="Bootstrap"
+                  className="w-12 h-12 object-contain"
+                />
+              ),
+              name: "Bootstrap",
+            },
+            {
+              icon: (
+                <img
+                  src="https://scriptverse.dev/img/tutorials/react-router.png"
+                  alt="React Router"
+                  className="w-12 h-12 object-contain"
+                />
+              ),
+              name: "React Router",
+            },
+            {
+              image:
+                "https://scalac.io/wp-content/uploads/2021/02/FB_Post-Size-link-image-size-5.jpg", // Kafka
+              name: "Kafka",
+            },
+            {
+              image:
+                "https://images.ctfassets.net/o7xu9whrs0u9/6qR4PTwCTfgl0tjfrz0lpy/819fecae70412dacb9b960b98f5fae3b/RabbitMQ-LOGO.png", // RabbitMQ
+              name: "RabbitMQ",
+            },
+            {
+              image: "https://saviynt.com/hubfs/aws.png", // AWS
+              name: "AWS (S3, RDS, Lambda, ECS)",
+            },
+            {
+              image:
+                "https://cdn.prod.website-files.com/6449405754e757db07f25327/6656429d1776bd09704334e8_google.webp", // GCP
+              name: "Google Cloud Platform (GCP)",
+            },
+            {
+              image:
+                "https://www.padok.fr/hubfs/Images/Azure/Microsoft-Azure.webp", // Azure
+              name: "Microsoft Azure",
+            },
+            {
+              image:
+                "https://www.ibm.com/content/dam/adobe-cms/instana/media_logo/Hystrix.component.complex-narrative-xl.ts=1690484720674.png", // Netflix Hystrix
+              name: "Netflix Hystrix",
+            },
+            {
+              image:
+                "https://huongdanjava.com/wp-content/uploads/2018/04/spring-cloud-netflix.jpeg", // Netflix Eureka
+              name: "Netflix Eureka",
+            },
+            {
+              icon: <SiDocker size={36} className="text-blue-500" />,
+              name: "Docker",
+            },
+            {
+              icon: <SiKubernetes size={36} className="text-blue-300" />,
+              name: "Kubernetes (K8s)",
+            },
+            {
+              icon: <SiPostgresql size={36} className="text-blue-500" />,
+              name: "PostgreSQL",
+            },
+            {
+              icon: <SiMysql size={36} className="text-orange-500" />,
+              name: "MySQL",
+            },
+            {
+              icon: <SiMongodb size={36} className="text-green-500" />,
+              name: "MongoDB",
+            },
+            {
+              icon: <SiHibernate size={36} className="text-purple-500" />,
+              name: "Hibernate",
+            },
+            {
+              icon: <SiSpringboot size={36} className="text-green-500" />,
+              name: "Spring Boot",
+            },
+          ].map((tech, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center space-y-2 p-4 rounded-lg bg-[#0f041a] hover:scale-105 transform transition-all duration-300 overflow-hidden"
+            >
+              {tech.image ? (
+                <img
+                  src={tech.image}
+                  alt={tech.name}
+                  className="w-12 h-12 object-contain"
+                />
+              ) : (
+                tech.icon
+              )}
+              <span className="font-medium text-gray-300">{tech.name}</span>
             </div>
-          </div>
-
-          {/* Backend Section */}
-          <div>
-            <h2 className="text-3xl font-bold mb-8 text-gray-200">
-              Backend Technologies
-            </h2>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 overflow-hidden">
-              {[
-                {
-                  icon: <SiSpringboot size={36} className="text-green-500" />,
-                  name: "Spring Boot",
-                },
-                {
-                  icon: <FaJava size={36} className="text-red-500" />,
-                  name: "Java",
-                },
-                {
-                  icon: <SiPostgresql size={36} className="text-blue-500" />,
-                  name: "PostgreSQL",
-                },
-                {
-                  icon: <SiMysql size={36} className="text-orange-500" />,
-                  name: "MySQL",
-                },
-                {
-                  icon: <SiMongodb size={36} className="text-green-500" />,
-                  name: "MongoDB",
-                },
-                {
-                  icon: <SiHibernate size={36} className="text-purple-500" />,
-                  name: "Hibernate",
-                },
-                {
-                  icon: <SiDocker size={36} className="text-blue-500" />,
-                  name: "Docker",
-                },
-                {
-                  icon: <SiKubernetes size={36} className="text-blue-300" />,
-                  name: "Kubernetes",
-                },
-              ].map((tech, index) => (
-                <div
-                  key={index}
-                  className="flex flex-col items-center space-y-2 p-4 rounded-lg bg-[#0f041a] hover:scale-105 transform transition-all duration-300 overflow-hidden"
-                >
-                  {tech.icon}
-                  <span className="font-medium text-gray-300">{tech.name}</span>
-                </div>
-              ))}
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
